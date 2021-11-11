24a0ad0ffe8e20ac5991aeb5d3813f1c28a29257
mkmrah <39188713+mkmrah@users.noreply.github.com>
2021-11-11 10:15:28 +0530

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -6,6 +6,7 @@
 
 
 resource "ibm_container_cluster" "classic_cluster" {
+  resource_group_id = "${data.ibm_resource_group.resource_group.id}"
   name            = var.cluster_name
   datacenter      = var.zones
   machine_type    = var.machine_type
