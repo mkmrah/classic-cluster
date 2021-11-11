05080fcff7bf414cf4648dcea6132ea168674fc7
mkmrah <39188713+mkmrah@users.noreply.github.com>
2021-11-11 10:14:16 +0530

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -3,35 +3,15 @@
 # Create Cluster
 ##############################################################################
 
-resource ibm_container_cluster classic_cluster {
 
-    name              = var.cluster_name
-    resource_group_id = var.resource_group_id
-    datacenter        = element(var.zones, 0)
-    machine_type      = var.machine_type
-    kube_version      = var.kube_version
-    hardware          = var.hardware
-    public_vlan_id    = var.private_only ? null : element(var.public_vlan_ids, 0)
-    private_vlan_id   = element(var.private_vlan_ids, 0)
-    default_pool_size = var.default_pool_size
 
+resource "ibm_container_cluster" "classic_cluster" {
+  name            = var.cluster_name
+  datacenter      = var.zones
+  machine_type    = var.machine_type
+  hardware        = var.hardware
+  default_pool_size = 1
 }
 
-#############################################################################
-
-
-#############################################################################
-# Create multizone attachments for cluster
-#############################################################################
-
-resource ibm_container_worker_pool_zone_attachment multi_zone {
-    count             = length(var.zones) - 1
-    resource_group_id = var.resource_group_id
-    cluster           = ibm_container_cluster.classic_cluster.name
-    worker_pool       = ibm_container_cluster.classic_cluster.worker_pools.0.id
-    zone              = element(var.zones, count.index + 1)
-    public_vlan_id    = var.private_only ? null : element(var.public_vlan_ids, count.index + 1)
-    private_vlan_id   = element(var.private_vlan_ids, count.index + 1)
-}
 
 #############################################################################
