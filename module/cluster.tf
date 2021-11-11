a2206fccfedcd64101f37ce917f135883071706d
mkmrah <39188713+mkmrah@users.noreply.github.com>
2021-11-11 10:08:59 +0530

    Update cluster.tf

--- module/cluster.tf
+++ module/cluster.tf
@@ -7,7 +7,7 @@ resource ibm_container_cluster classic_cluster {
 
     name              = "${var.cluster_name}"
     resource_group_id = "${data.ibm_resource_group.resource_group.id}"
-    datacenter        = "${element(var.zones, 0)}"
+    datacenter        = "${var.zones)}"
     machine_type      = "${var.machine_type}"
     kube_version      = "${var.kube_version}"
     hardware          = "${var.hardware}"
@@ -17,17 +17,3 @@ resource ibm_container_cluster classic_cluster {
 
 #############################################################################
 
-
-#############################################################################
-# Create multizone attachments for cluster
-#############################################################################
-
-resource ibm_container_worker_pool_zone_attachment multi_zone {
-    count             = "${length(var.zones) - 1}"
-    resource_group_id = "${data.ibm_resource_group.resource_group.id}"
-    cluster           = "${ibm_container_cluster.classic_cluster.name}"
-    worker_pool       = "${ibm_container_cluster.classic_cluster.worker_pools.0.id}"
-    zone              = "${element(var.zones, count.index + 1)}"
-}
-
-#############################################################################
