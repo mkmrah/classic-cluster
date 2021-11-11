70045866b59163c0380b279b266be68f827b720d
mkmrah <39188713+mkmrah@users.noreply.github.com>
2021-11-11 10:15:37 +0530

    Update cluster.tf

--- module/cluster.tf
+++ module/cluster.tf
@@ -3,16 +3,16 @@
 # Create Cluster
 ##############################################################################
 
-resource ibm_container_cluster classic_cluster {
 
-    name              = "${var.cluster_name}"
-    resource_group_id = "${data.ibm_resource_group.resource_group.id}"
-    datacenter        = "${var.zones)}"
-    machine_type      = "${var.machine_type}"
-    kube_version      = "${var.kube_version}"
-    hardware          = "${var.hardware}"
-    default_pool_size = "${var.default_pool_size}"
 
+
+resource "ibm_container_cluster" "classic_cluster" {
+  resource_group_id = "${data.ibm_resource_group.resource_group.id}"
+  name            = var.cluster_name
+  datacenter      = var.zones
+  machine_type    = var.machine_type
+  hardware        = var.hardware
+  default_pool_size = 1
 }
 
 #############################################################################
